6e0e81b64abdc026f3b48c2d9329f1d28fd71584
Bay <baybui112@gmail.com>
2021-03-20 14:04:31 -0700

    finished up search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -89,4 +90,33 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
+//function to accept language parameter
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+function buttonClickHandler(e) {
+  var clickedLanguage = e.target.getAttribute("data-language");
+  if (clickedLanguage) {
+    getFeaturedRepos(clickedLanguage);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
